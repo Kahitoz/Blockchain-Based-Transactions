e4c55231d7970c9bdd82c47c2243a1dacbf6d2e7
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-04 19:03:52 +0530

    updated to allow specific origin

--- BlockChainServer/Server/main.py
+++ BlockChainServer/Server/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from databasFunc import get_sercet_key, get_account_details
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:3000"]
+)
 
 @app.get("/get_secret/{key}")
 def get_secret(key: str):
