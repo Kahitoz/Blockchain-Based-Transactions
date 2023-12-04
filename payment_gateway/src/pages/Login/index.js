21944ea3893da790e7af82c033afefc03f94bd5f
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-04 19:22:21 +0530

    updated

--- payment_gateway/src/pages/Login/index.js
+++ payment_gateway/src/pages/Login/index.js
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Col, Form,Row } from "antd";
-function Login() {
-    const navigate = useNavigate();
-    const onFinish = async (values) => {
-        console.log("data recived : ", values);
-        
-    }
-    return (
-        <div className="bg-gradient flex items-center justify-center h-screen">
-            <div className="card w-400 p-2">
-            <div className="flex items-center justify-between">
-            <h1 className="text-2xl">KYŌMI - EXISTING USER</h1>
+import { Col, Form, Row } from "antd";
+import { Log_in_user } from "../../Logic/LoginLogic";
+const Login = () => {
+  const [key, setKey] = useState("");
+  const navigate = useNavigate();
+
+  const handleKeyChange = (e) =>{
+    setKey(e.target.value);
+  }
+
+  const HandleOnclickButtonChange = async(value)=>{
+    await Log_in_user(value);
+    
+  }
+  return (
+    <div className="bg-gradient flex items-center justify-center h-screen">
+      <div className="card w-400 p-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl">KYŌMI - EXISTING USER</h1>
         </div>
-        <hr/>
-        <Form layout="vertical" onFinish={onFinish}>
+        <hr />
+        <Form layout="vertical">
           <Row gutter={16}>
             <Col span={24}>
               <Form.Item label="PRIVATE-KEY" name="PRIVATE-KEY">
-                <input type="text" />
+                <input type="text" 
+                value = {key}
+                placeholder="Your Private Key"
+                onChange={handleKeyChange}/>
               </Form.Item>
             </Col>
-
-            
           </Row>
 
-          <button className="primary-contained-btn w-100" type="submit">
+          <button className="primary-contained-btn w-100" type="submit" onClick={()=>HandleOnclickButtonChange(key)}>
             Login
           </button>
           <h1
@@ -34,13 +42,10 @@ function Login() {
           >
             Not a member , Click Here To Register
           </h1>
-         
         </Form>
-            </div>
-            </div>
-    );
-}
-
-
+      </div>
+    </div>
+  );
+};
 
 export default Login;
