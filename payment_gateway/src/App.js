29ec4c231fbcc1d655cd3f04243a575257891778
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-04 20:41:35 +0530

    added dashboard route

--- payment_gateway/src/App.js
+++ payment_gateway/src/App.js
@@ -8,6 +8,8 @@ import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard/Dashboard";
+
 function App() {
   return (
     <div >
@@ -15,7 +17,7 @@ function App() {
          <Routes>
           <Route path="*" element={<Login/>}/>,
           <Route path="/register" element={<Register/>}/>
-       
+          <Route path="/dashboard" element={<Dashboard/>}/>
          </Routes>
          </BrowserRouter>
     </div>
